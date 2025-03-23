54d3e9a4be4170b88b8b43dc0e871cabc6246253
AnkushAtCryptoSkool <cryptoskool@gmail.com>
2025-03-23 17:41:03 +0530

    next code

---
+++ src/EncodeDecode.java
@@ -0,0 +1,35 @@
+package src;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class EncodeDecode {
+    public static void main(String[] args) {
+        List<String> s = Arrays.asList("abc","def");
+        encodeStr(s);
+        decodeStr(encodeStr(s));
+    }
+
+    private static String encodeStr(List<String> s) {
+      StringBuilder sb = new StringBuilder();
+      for (String str : s){
+          sb.append(str.length()).append("#").append(str);
+      }
+        System.out.println(sb);
+      return sb.toString();
+    }
+    private static void decodeStr(String s) {
+      int i = 0;
+      List<String> str = new ArrayList<>();
+      while(i<s.length()){
+          int hashIdx = s.indexOf("#",i);
+          int length = Integer.valueOf(s.substring(i,hashIdx));
+          i = hashIdx+1;
+          String temp = s.substring(i,i+length);
+          str.add(temp);
+          i = i+length;
+      }
+        System.out.println(str);
+    }
+}
---
+++ src/LongestConsecutive.java
@@ -0,0 +1,40 @@
+package src;
+
+import java.util.HashSet;
+
+public class LongestConsecutive {
+    public static void main(String[] args) {
+        LongestConsecutive longestConsecutive = new LongestConsecutive();
+        int arr[] = {1,2,3,45,6,4};
+        System.out.println(longestConsecutive.longestConsecutive(arr));
+    }
+
+    public int longestConsecutive(int[] nums) {
+        if (nums.length == 0) return 0; // Edge case
+
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        int maxLen = 0;
+
+        for (int num : nums) {
+            // Check if num is the start of a sequence
+            if (!set.contains(num - 1)) {
+                int currentNum = num;
+                int currentLen = 1;
+
+                // Count consecutive numbers
+                while (set.contains(currentNum + 1)) {
+                    currentNum++;
+                    currentLen++;
+                }
+
+                maxLen = Math.max(maxLen, currentLen);
+            }
+        }
+
+        return maxLen;
+    }
+}
